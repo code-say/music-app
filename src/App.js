48650784c2861e3f232e1aa96dca5a70c29c9344
sayan <sayanmandal8643@gmail.com>
2021-09-03 06:06:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+//Import Styles
+import './styles/app.scss';
+//Adding Components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from './components/Library';
+//import util
+import data from "./util";
 
 function App() {
+  //state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setcurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song currentSong={currentSong}/>
+      <Player 
+      setIsPlaying={setIsPlaying}
+      isPlaying={isPlaying}
+      currentSong={currentSong}
+      />
+      <Library songs={songs} setcurrentSong={setcurrentSong} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/util.js
@@ -0,0 +1,35 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+    return [
+        {
+            name: "By Chance",
+            artist: "SwuM",
+            cover:
+            "https://chillhop.com/wp-content/uploads/2021/03/74d62bc9370a68e440c1b98eaf650344f0a7faea-1024x1024.jpg",
+            id: uuidv4(),
+            active: true,
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=15224"
+        },
+        {
+            name:"Cruising",
+            artist: "Evil Needle",
+            cover:
+            "https://chillhop.com/wp-content/uploads/2021/04/cb0cc6270d7f2e1bb13e44e8832bd5c9b2a61080-1024x1024.jpg",
+            id: uuidv4(),
+            active: false,
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=17087"
+        },
+        {
+            name:"Runaway",
+            artist:"Blue Wednesday, Magnus Klausen",
+            cover:
+            "https://chillhop.com/wp-content/uploads/2020/09/5bff1a6f1bd0e2168d29b4c841b811598135e457-1024x1024.jpg",
+            id: uuidv4(),
+            active: true,
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=12134"
+        }
+    ];
+}
+
+export default chillHop;
